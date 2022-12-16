fdc8a101a68be1e8f809cb3645ab4162daa8012b
takuya_kajiwara <fd.elt.119@gmail.com>
2022-12-16 17:28:56 +0900

    スロット

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 
 class ComponentTestController extends Controller
 {
-    public function showComponent1(){
+    public function showComponent1()
+    {
         return view('tests.component-test1');
     }
-    public function showComponent2(){
+    public function showComponent2()
+    {
         return view('tests.component-test2');
     }
 }
