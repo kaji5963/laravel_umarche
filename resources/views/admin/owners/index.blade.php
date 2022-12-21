39f2d13ffb3e2b2f839009170702fc94e45bd3c5
takuya_kajiwara <fd.elt.119@gmail.com>
2022-12-22 06:44:15 +0900

    image delete

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -52,12 +52,12 @@
                                                     action="{{ route('admin.owners.destroy', ['owner' => $owner->id]) }}">
                                                     @csrf
                                                     @method('delete')
+                                                    <td class="px-4 py-3">
+                                                        <a href="#" data-id="{{ $owner->id }}"
+                                                            onclick="deletePost(this)"
+                                                            class=" text-white bg-red-300 border-0 py-2 px-4 focus:outline-none hover:bg-red-400 rounded">削除</a>
+                                                    </td>
                                                 </form>
-                                                <td class="px-4 py-3">
-                                                    <a href="#" data-id="{{ $owner->id }}"
-                                                        onclick="deletePost(this)"
-                                                        class=" text-white bg-red-300 border-0 py-2 px-4 focus:outline-none hover:bg-red-400 rounded">削除</a>
-                                                </td>
                                             </tr>
                                         @endforeach
                                     </tbody>
