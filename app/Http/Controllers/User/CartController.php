12092ee89bb276c2e696ce617fd60e76ac37258c
takuya_kajiwara <fd.elt.119@gmail.com>
2022-12-26 06:02:53 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -4,19 +4,34 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 
 class CartController extends Controller
 {
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->product;
+        $totalPrice = 0;
+
+        foreach ($products as $product) {
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+        // dd($product, $totalPrice);
+        return view('user.cart', compact('products', 'totalPrice'));
+    }
+
+
     public function add(Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)
             ->where('user_id', Auth::id())
             ->first();
 
-        if($itemInCart) {
+        if ($itemInCart) {
             $itemInCart->quantity += $request->quantity;
             $itemInCart->save();
         } else {
@@ -26,6 +41,6 @@ class CartController extends Controller
                 'quantity' => $request->quantity,
             ]);
         }
-        dd('test');
+        return redirect()->route('user.cart.index');
     }
 }
